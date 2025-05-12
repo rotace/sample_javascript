e452e637a98b8aced40fab87cf673d9859145397
rotace <rotace2012227ys@gmail.com>
2025-05-12 22:34:59 +0900

    vuetifyの導入

--- vue.js/sample_vuetify/src/main.js
+++ vue.js/sample_vuetify/src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
+// Material Design Icon
+import '@mdi/font/css/materialdesignicons.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'dark',
+    }
+})
 
 createApp(App).mount('#app')
--- vue.js/sample_vuetify/vite.config.js
+++ vue.js/sample_vuetify/vite.config.js
@@ -2,12 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify({ autoImport: true, }),
     vueDevTools(),
   ],
   resolve: {
