ff6bc52fda6f7f01a9f3ec0a3cf977d4418c4fc5
rotace <rotace2012227ys@gmail.com>
2025-05-13 21:06:24 +0900

    リンタ、フォーマッタの開発プラグインを追加

--- vue.js/redmine_frontend/eslint.config.js
+++ vue.js/redmine_frontend/eslint.config.js
@@ -1,1 +1,12 @@
-export { default } from 'eslint-config-vuetify'
+import { FlatCompat } from '@eslint/eslintrc';
+export { vuetify } from 'eslint-config-vuetify'
+
+const compat = new FlatCompat();
+export default [
+    vuetify(),
+    ...compat.extends(
+        'plugin:vue/vue3-essential',
+        'eslint/recommended',
+        'prettier'
+    ),
+];
